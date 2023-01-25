12b5c7d6eb67ef0fc6e8a4fcf429b1863f164b70
mumanski <58386912+maciej-umanski@users.noreply.github.com>
2023-01-25 16:30:14 +0100

    feat: delete samba service

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 USERNAME=$1
-echo "CONFIGURING SYSTEM FOR USERNAME: $USERNAME"
 
 # Disable MOTD
+########################################################################################################################
 touch /home/"$USERNAME"/.hushlogin
 
 # Debloat APT
+########################################################################################################################
 tee -a /etc/apt/apt.conf.d/99_norecommends > /dev/null <<EOT
 APT::Install-Suggests "0";
 APT::Install-Recommends "0";
@@ -17,11 +18,13 @@ EOT
 apt update && apt upgrade -y && apt autoremove -y && apt autoclean -y
 
 # Enable APT repositories
+########################################################################################################################
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -r -i 's/^deb(.*)$/deb\1 contrib non-free/g' /etc/apt/sources.list
 apt update
 
 # Install Docker
+########################################################################################################################
 apt update && apt install ca-certificates curl gnupg lsb-release -y
 
 mkdir -p /etc/apt/keyrings
@@ -36,7 +39,9 @@ apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-c
 usermod -aG docker "$USERNAME"
 
 # Install needed software
-apt update && apt install openssh-server vim firmware-linux-nonfree amd64-microcode git -y
+########################################################################################################################
+apt update && apt install openssh-server vim firmware-linux-nonfree amd64-microcode git firmware-realtek -y
 
 # Pull this repository
-git pull https://github.com/maciej-umanski/homeserver.git
+########################################################################################################################
+git clone https://github.com/maciej-umanski/homeserver.git
